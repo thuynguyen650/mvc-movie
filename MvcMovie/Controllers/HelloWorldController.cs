0404f84672382d58ba57c8ceaff80d5a8ab7dce7
nthuy <thuynguyen.webdev@gmail.com>
2023-05-03 08:43:50 +0700

    add helloworld controller

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+
+    public string Welcome(string name, int id = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello World: {name}, NumTimes is {id}");
+    }
+}
